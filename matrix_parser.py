962ffbe446d9016934840e5814dee372cd5c0a4c
Carlos Maniero <carlosmaniero@gmail.com>
2016-06-15 08:48:54 -0300

    Start parser method init

---
+++ matrix_parser.py
@@ -0,0 +1,19 @@
+from matrix import Matrix
+
+
+class MatrixParser:
+
+    def init(self, m, n):
+        m, n = int(m), int(n)
+        self.matrix = Matrix(m, n)
+
+    def parse(self, command):
+        command, *args = command.split()
+        method = self._REGISTER.get(command)
+
+        if method:
+            method(self, *args)
+
+    _REGISTER = {
+        'I': init
+    }
